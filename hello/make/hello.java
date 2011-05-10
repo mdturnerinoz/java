3d98f07aaadd51b2e5c6b1c81a0c4248a837d3bc
Marty Turner <mdturnerinoz@gmail.com>
2011-05-11 08:24:33 +1000

    changes

--- hello/make/hello.java
+++ hello/make/hello.java
@@ -2,8 +2,7 @@ class hello
 {  
         public static void main(String args[])
        {
-           System.out.println("\n\tHello git1 World!\n\n");
-           System.out.println("\n\tHello git2 World!\n\n");
+           System.out.println("\n\tHello java jar World!\n\n");
        }
 }
 
