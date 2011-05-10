e41f3e48bf06721c1d5ec294a285d9a120d6f9a8
Marty Turner <mdturnerinoz@gmail.com>
2011-05-11 08:18:19 +1000

    add hello hava

---
+++ hello/make-dirs/src/hello.java
@@ -0,0 +1,8 @@
+package build;
+class hello
+{  
+        public static void main(String args[])
+       {
+           System.out.println("\n\tHello World!\n\n");
+       }
+}
---
+++ hello/make/hello.java
@@ -0,0 +1,9 @@
+class hello
+{  
+        public static void main(String args[])
+       {
+           System.out.println("\n\tHello git1 World!\n\n");
+           System.out.println("\n\tHello git2 World!\n\n");
+       }
+}
+
