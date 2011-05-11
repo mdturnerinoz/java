2a38086fb86a0a00f3e3bd67ddb7f7c9d6b62dba
Marty Turner <mdturnerinoz@gmail.com>
2011-05-11 10:38:04 +1000

    final fixups

--- hello/make-dirs/src/hello.java
+++ hello/make-dirs/src/hello.java
@@ -3,6 +3,6 @@ class hello
 {  
         public static void main(String args[])
        {
-           System.out.println("\n\tHello World!\n\n");
+           System.out.println("\n\tHello Java Jar Dirs World!\n\n");
        }
 }
